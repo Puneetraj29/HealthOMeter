da4fbe3fa29894f99f7857097478f27aca57e1e0
Puneetraj29 <60169343+Puneetraj29@users.noreply.github.com>
2021-01-08 00:15:25 +0530

    Create result.py

---
+++ result/result.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for, flash, redirect
+import joblib
+from flask import request
+import numpy as np
+
+
+def cancer():
+    return render_template("result.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
